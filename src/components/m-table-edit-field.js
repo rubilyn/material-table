73199d9c8e3a0c17355b5c4bd4543b1a88a41f9f
Rubilyn <rubilyn@gmail.com>
2020-04-29 21:23:03 -0700

    fix invalid prop error on header title

--- src/components/m-table-edit-field.js
+++ src/components/m-table-edit-field.js
@@ -107,7 +107,7 @@ class MTableEditField extends React.Component {
         {...this.getProps()}
         style={this.props.columnDef.type === 'numeric' ? { float: 'right' } : {}}
         type={this.props.columnDef.type === 'numeric' ? 'number' : 'text'}
-        placeholder={this.props.columnDef.title}
+        placeholder={(typeof this.props.columnDef.title !== 'string') ? this.props.columnDef.placeholder : this.props.columnDef.title}
         value={this.props.value === undefined ? '' : this.props.value}
         onChange={event => this.props.onChange(event.target.value)}
         InputProps={{
